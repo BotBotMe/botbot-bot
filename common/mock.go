59801dc6a069df35832d13b2c794b4c2695a5b69
Vitaly Babiy <vbabiy86@gmail.com>
2014-06-19 12:46:24 -0400

    Fixing mock irc server to handle tls connect correctly.

--- common/mock.go
+++ common/mock.go
@@ -32,36 +32,43 @@ func (self *MockIRCServer) Run(t *testing.T) {
 		return
 	}
 
-	// Accept a single connection
-	conn, lerr := listener.Accept()
-	if lerr != nil {
-		t.Error("Error on IRC server on Accept. ", err)
-	}
-
-	// First message triggers BotBot to send USER and NICK messages
-	conn.Write([]byte(":hybrid7.debian.local NOTICE AUTH :*** Looking up your hostname...\n"))
-
-	// Ask for NickServ auth, and pretend we got it
-	conn.Write([]byte(":NickServ!NickServ@services. NOTICE graham_king :This nickname is registered. Please choose a different nickname, or identify via /msg NickServ identify <password>\n"))
-	conn.Write([]byte(":NickServ!NickServ@services. NOTICE graham_king :You are now identified for graham_king.\n"))
-
-	conn.Write([]byte(":wolfe.freenode.net 001 graham_king :Welcome to the freenode Internet Relay Chat Network graham_king\n"))
-
-	// This should get sent to plugins
-	conn.Write([]byte(":yml!~yml@li148-151.members.linode.com PRIVMSG #unit :" + self.Message + "\n"))
-	//conn.Write([]byte("test: " + self.Message + "\n"))
-
-	var derr error
-	var data []byte
-
-	bufRead := bufio.NewReader(conn)
 	for {
-		data, derr = bufRead.ReadBytes('\n')
-		if derr != nil {
-			// Client closed connection
-			break
+		conn, lerr := listener.Accept()
+		// If create a new connection throw the old data away
+		// This can happen if a client trys to connect with tls
+		// Got will store the handshake data. The cient will try
+		// connect with a plaintext connect after the tls fails.
+		self.Got = make([]string, 0)
+
+		if lerr != nil {
+			t.Error("Error on IRC server on Accept. ", err)
 		}
-		self.Got = append(self.Got, string(data))
+
+		// First message triggers BotBot to send USER and NICK messages
+		conn.Write([]byte(":hybrid7.debian.local NOTICE AUTH :*** Looking up your hostname...\n"))
+
+		// Ask for NickServ auth, and pretend we got it
+		conn.Write([]byte(":NickServ!NickServ@services. NOTICE graham_king :This nickname is registered. Please choose a different nickname, or identify via /msg NickServ identify <password>\n"))
+		conn.Write([]byte(":NickServ!NickServ@services. NOTICE graham_king :You are now identified for graham_king.\n"))
+
+		conn.Write([]byte(":wolfe.freenode.net 001 graham_king :Welcome to the freenode Internet Relay Chat Network graham_king\n"))
+
+		// This should get sent to plugins
+		conn.Write([]byte(":yml!~yml@li148-151.members.linode.com PRIVMSG #unit :" + self.Message + "\n"))
+		//conn.Write([]byte("test: " + self.Message + "\n"))
+
+		var derr error
+		var data []byte
+
+		bufRead := bufio.NewReader(conn)
+		for {
+			data, derr = bufRead.ReadBytes('\n')
+			if derr != nil {
+				// Client closed connection
+				break
+			}
+			self.Got = append(self.Got, string(data))
+		}
 	}
 
 }
